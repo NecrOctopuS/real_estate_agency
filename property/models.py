a893a167bd0f7e68785c5a7011ab75f0d849de45
NecrOctopuS <53893624+NecrOctopuS@users.noreply.github.com>
2019-11-18 17:14:36 +0300

    Normalize Flat.phonenumbers into Flat.owner_phone_pure

--- property/models.py
+++ property/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
@@ -24,6 +25,7 @@ class Flat(models.Model):
     construction_year = models.IntegerField("Год постройки здания", null=True, blank=True, db_index=True)
     new_building = models.NullBooleanField('Новостройка', )
     liked_by = models.ManyToManyField(User, related_name="liked_flats", verbose_name='Кто лайкнул', blank=True)
+    owner_phone_pure = PhoneNumberField(blank=True, verbose_name='Нормализированный номер владельца')
 
     def __str__(self):
         return f"{self.town}, {self.address} ({self.price}р.)"
